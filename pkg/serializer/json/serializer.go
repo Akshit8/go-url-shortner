35a5f7ea998bcc7127cf05736899ad6193abd4bb
Akshit Sadana <akshitsadana@gmail.com>
2021-02-20 23:36:32 +0530

    refractor 2

---
+++ pkg/serializer/json/serializer.go
@@ -0,0 +1,29 @@
+package json
+
+import (
+	"encoding/json"
+
+	"github.com/Akshit8/url-shortner/pkg/url"
+	errs "github.com/pkg/errors"
+)
+
+// Serializer binds encode-decode methods
+type Serializer struct{}
+
+// Decode decodes byte array to json
+func (s *Serializer) Decode(input []byte) (*url.URL, error) {
+	redirect := &url.URL{}
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errs.Wrap(err, "serializer.json.Decode")
+	}
+	return redirect, nil
+}
+
+// Encode encodes a go struct to byte array
+func (s *Serializer) Encode(input *url.URL) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errs.Wrap(err, "serializer.json.Encode")
+	}
+	return rawMsg, nil
+}
